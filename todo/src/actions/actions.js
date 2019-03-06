1ea390b195ba91a75f94557b90fb202f350ee2d4
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-05 20:13:58 -0500

    Cleaned up code a bit.

--- todo/src/actions/actions.js
+++ todo/src/actions/actions.js
@@ -1,35 +1,12 @@
-import uuid from 'uuid';
-
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED';
 export const DELETE_TODO = 'DELETE_TODO';
 export const DELETE_COMPLETED = 'DELETE_COMPLETED';
 
-export const addTodo = todo => {
-  return {
-    type: ADD_TODO,
-    payload: {
-      id: uuid.v4(),
-      value: todo,
-      completed: false
-    }
-  };
-};
+export const addTodo = todo => ({ type: ADD_TODO, payload: todo });
 
-export const toggleCompleted = id => {
-  return {
-    type: TOGGLE_COMPLETED,
-    payload: id
-  };
-};
+export const toggleCompleted = id => ({ type: TOGGLE_COMPLETED, payload: id });
 
-export const deleteTodo = id => {
-  return {
-    type: DELETE_TODO,
-    payload: id
-  };
-};
+export const deleteTodo = id => ({ type: DELETE_TODO, payload: id });
 
-export const deleteCompleted = () => {
-  return { type: DELETE_COMPLETED };
-};
+export const deleteCompleted = () => ({ type: DELETE_COMPLETED });
